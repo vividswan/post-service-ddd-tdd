d0c9ee9ce2197d9357f033f1537133b9e4060c5f
vividswan <vividswan@naver.com>
2024-02-01 20:39:23 +0900

    feat: 초기 유저 Entity 생성

---
+++ src/main/java/com/vividswan/postservice/module/user/domain/User.java
@@ -0,0 +1,16 @@
+package com.vividswan.postservice.module.user.domain;
+
+import com.vividswan.postservice.common.domain.DomainEntity;
+
+class UserProps {
+	private String nickname;
+	private String email;
+	private String password;
+
+}
+
+public class User extends DomainEntity<UserProps> {
+	public User(Long id, UserProps props) {
+		super(id, props);
+	}
+}
