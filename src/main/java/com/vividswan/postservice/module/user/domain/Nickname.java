7d5ff77c0055b36357b45b1f5579d7366f2176e1
vividswan <vividswan@naver.com>
2024-02-04 17:46:42 +0900

    feat: Nickname VO 클래스 예외처리 관련 변경

--- src/main/java/com/vividswan/postservice/module/user/domain/Nickname.java
+++ src/main/java/com/vividswan/postservice/module/user/domain/Nickname.java
@@ -1,6 +1,28 @@
 package com.vividswan.postservice.module.user.domain;
 
+import static com.vividswan.postservice.module.user.domain.Nickname.NicknameValidationError.*;
+
+import lombok.Getter;
+
 public class Nickname {
+
+	public static final int minLength = 4;
+	public static final int maxLength = 10;
+
+	public enum NicknameValidationError {
+		NULL_VALUE("닉네임 값이 null 입니다."),
+		TOO_SHORT("닉네임은 " + minLength + "글자 이상이여야합니다."),
+		TOO_LONG("닉네임은 " + maxLength + "글자 이하여야합니다.");
+
+		@Getter
+		private final String message;
+
+		NicknameValidationError(String message) {
+			this.message = message;
+		}
+	}
+
+	@Getter
 	private final String value;
 
 	private Nickname(String value) {
@@ -14,11 +36,11 @@ public class Nickname {
 
 	private void validateNickname() {
 		if (this.value == null) {
-			throw new IllegalArgumentException("닉네임 값이 null 입니다.");
-		} else if (this.value.length() < 3) {
-			throw new IllegalArgumentException("닉네임은 4글자 이상이여야합니다.");
-		} else if (this.value.length() > 10) {
-			throw new IllegalArgumentException("닉네임은 10글자 이하여야합니다.");
+			throw new IllegalArgumentException(NULL_VALUE.getMessage());
+		} else if (this.value.length() < minLength) {
+			throw new IllegalArgumentException(TOO_SHORT.getMessage());
+		} else if (this.value.length() > maxLength) {
+			throw new IllegalArgumentException(TOO_LONG.getMessage());
 		}
 	}
 }
